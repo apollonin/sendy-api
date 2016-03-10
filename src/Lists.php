d2fac46395dabf910d4dd24192ff6a3c73b8002d
Dmitriy Apollonin <dmitriy.apollonin@unisend.co>
2016-03-10 12:48:00 +0200

    add lust add nethod

--- src/Lists.php
+++ src/Lists.php
@@ -7,6 +7,44 @@
 
 $app->group('/lists', function() use ($app, $db) {
 
+    $app->get('/add', function() use ($app, $db) {
+
+        $get = $app->request->get();
+
+        if(!$get['name'] )
+        {
+            echo json_encode(array('status' => 'error', 'result' => 'Some parameters are missing'));
+            $app->stop();
+        }
+
+        //get app_key
+        $sth = $db->prepare('SELECT id FROM apps WHERE app_key = :app_key');
+        $sth->execute(array('app_key' => $app->request()->get('app_key')));
+        $app = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+        //check if this list already exists
+        $sth = $db->prepare('SELECT * FROM lists WHERE name = :name');
+        $sth->execute(array('name' => $get['name']));
+        $res = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+        //don't add new list, just return existed
+        if(count($res) > 0)
+        {
+            echo json_encode(array('status' => 'ok', 'result' => $res[0]['id']));
+            $app->stop();
+        }
+
+        $sth = $db->prepare('INSERT INTO lists SET name = :name, app = :app');
+
+        $sth->execute(array(
+                        'name' => $get['name'],
+                        'app'  => $app[0]['id'],
+                        ));
+
+        echo json_encode(array('status' => 'ok', 'result' => $db->lastInsertId()));
+
+    });
+
     $app->get('/get', function() use ($app, $db) {
 
         if(!$app->request()->get('name'))
